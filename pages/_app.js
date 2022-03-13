2275bdb974c42e2fb0791abed745bd4138d21fcb
Felipe Franklin <felipelfbezerra54@gmail.com>
2022-03-13 14:11:03 -0300

    page title fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          <title>Shopping cart</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
